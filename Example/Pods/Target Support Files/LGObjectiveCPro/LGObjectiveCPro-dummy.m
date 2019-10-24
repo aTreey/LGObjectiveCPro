4953f9fb5408af6f2e2733e66fe1df2a9f9d4924
480814177 <480814177@qq.com>
2019-10-24 15:30:40 +0800

    初始化

---
+++ Example/Pods/Target Support Files/LGObjectiveCPro/LGObjectiveCPro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LGObjectiveCPro : NSObject
+@end
+@implementation PodsDummy_LGObjectiveCPro
+@end
---
+++ Example/Pods/Target Support Files/Pods-LGObjectiveCPro_Tests/Pods-LGObjectiveCPro_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LGObjectiveCPro_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LGObjectiveCPro_Tests
+@end
